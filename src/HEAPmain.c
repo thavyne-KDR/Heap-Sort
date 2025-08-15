f68c3c548bd49c86170dc498addf7b390131a2de
thavyne-KDR <thavyne.kdr@example.com>
2025-08-14 21:03:48 -0300

    Ajusta apresentação dos resultados no terminal (vetor original e ordenado em linhas separadas)

--- src/HEAPmain.c
+++ src/HEAPmain.c
@@ -4,23 +4,27 @@
 int main()
 {
   int n;
-  printf("Digite o tamanho do vetor: ");
+  printf("==============================\n");
+  printf("     Algoritmo Heap Sort      \n");
+  printf("==============================\n");
+  printf("\nInforme o tamanho do vetor: ");
   scanf("%d", &n);
 
   int arr[n];
-  printf("Digite os elementos do vetor desordenado:\n");
+  printf("Digite os %d elementos do vetor:\n> ", n);
   for (int i = 0; i < n; i++)
   {
     scanf("%d", &arr[i]);
   }
 
-  printf("Vetor original: ");
+  printf("\nVetor original:\n");
   printArray(arr, n);
 
   heapSort(arr, n);
 
-  printf("Vetor ordenado pelo Heap Sort: ");
+  printf("Vetor ordenado pelo Heap Sort:\n");
   printArray(arr, n);
 
+  printf("\nProcesso concluido com sucesso.\n");
   return 0;
 }
